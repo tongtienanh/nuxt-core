f317a8212472150347be2e7f0a78ae9aa75fa0c9
TongAnh <anhtt@vuihoc.vn>
2022-12-28 23:11:08 +0700

    page detail

--- pages/[name].vue
+++ pages/[name].vue
@@ -5,12 +5,148 @@
   </div>
   <div class="flex">
     <div class="left w-[70%]">
-      <div class="w-full mt-2">
-        <img src="https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Fheather%2Fhome%2FEGS_RockstarGames_RedDeadRedemption2_G1A_00-1920x1080-308f101576da37225c889173094f373f2afc56c1.jpg">
+      <div class="w-full mb-3 mt-2">
+        <img
+          src="https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Fheather%2Fhome%2FEGS_RockstarGames_RedDeadRedemption2_G1A_00-1920x1080-308f101576da37225c889173094f373f2afc56c1.jpg">
       </div>
+      <h2 class="mb-5 uppercase" style="border-bottom: 2px solid rgba(255, 255, 255, 0.3)">Giới thiệu game Red Dead
+        Redemption 2</h2>
+      <p class="mb-[1.5rem]"><strong>The Witcher 3: Wild Hunt </strong>là một trong những tựa game nhập vai hay nhất mọi
+        thời đại. Game do CD Project RED phát triển. Ngày 19 tháng 5 năm 2015, <strong>The Witcher 3</strong> được phát
+        hành trên PC, PS4 và Xbox One bởi Bandai Namco Entertainment.</p>
+      <p class="mb-[1.5rem]">Bài viết này sẽ thông tin đến bạn cấu hình tối thiểu và cấu hình đề nghị của<strong> The
+        Witcher 3: Wild Hunt</strong>. Bên cạnh đó là lối chơi, một vài hình ảnh trong game và đánh giá chung từ cộng
+        đồng</p>
+      <p class="mb-[1.5rem]">Có nhiều nhiệm vụ khác nhau trong <strong>The Witcher 3</strong>, nhưng chủ yếu xoay quanh
+        cuộc đối đầu của <strong>Geralt </strong>cùng những người bạn với một thế lực hùng mạnh tên là <strong>Wild
+          Hunt </strong>đến từ một thế giới khác. Đội quân này có dã tâm bành trướng thế giới. <strong>Wild
+          Hunt </strong>do tên thủ lãnh người Elf tên là <strong>Eredin </strong>cầm đầu.</p>
     </div>
-    <div class="right w-[30%]">
-      <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 w-full">Download</button>
+    <div class="right w-[30%] ml-4">
+      <div>
+        <div class="flex">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+               stroke="currentColor"
+               class="w-6 h-6 mr-1.5 text-red-700">
+            <path stroke-linecap="round" stroke-linejoin="round"
+                  d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"/>
+          </svg>
+          <h2 class="text-red-700">Chi tiết game Red Dead Redemption 2</h2>
+        </div>
+        <div class="mt-[10px]">
+          <div class="flex flex-col w-full">
+            <div class="py-[10px]" style="border-bottom: 1px solid rgba(255, 255, 255, 0.1);}">
+              <div class="flex justify-between">
+                <span>Kích thước</span>
+                <div>
+                  40.7GB
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="flex flex-col w-full">
+            <div class="py-[10px]" style="border-bottom: 1px solid rgba(255, 255, 255, 0.1);}">
+              <div class="flex justify-between">
+                <span>Số người chơi</span>
+                <div>
+                  Chơi đơn
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="flex flex-col w-full">
+            <div class="py-[10px]" style="border-bottom: 1px solid rgba(255, 255, 255, 0.1);}">
+              <div class="flex justify-between">
+                <span>Ngày update</span>
+                <div>
+                  28-12-2022
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="flex flex-col w-full">
+            <div class="py-[10px]" style="border-bottom: 1px solid rgba(255, 255, 255, 0.1);}">
+              <div class="flex justify-between">
+                <span>Thể loại</span>
+                <div>
+                  Hành động, phiêu lưu
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="mt-4">
+        <div class="flex">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-1.5 text-red-700">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z" />
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+          </svg>
+          <h2 class="text-red-700">Cấu hình tối thiểu</h2>
+        </div>
+        <div class="mt-[10px]">
+          <div class="flex flex-col w-full">
+            <div class="py-[10px]" style="border-bottom: 1px solid rgba(255, 255, 255, 0.1);}">
+              <div class="flex justify-between">
+                <span>OS</span>
+                <div>
+                  Windows 10
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="flex flex-col w-full">
+            <div class="py-[10px]" style="border-bottom: 1px solid rgba(255, 255, 255, 0.1);}">
+              <div class="flex justify-between">
+                <span>Processor</span>
+                <div>
+                  Core i5-4690K, AMD FX-8350
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="flex flex-col w-full">
+            <div class="py-[10px]" style="border-bottom: 1px solid rgba(255, 255, 255, 0.1);}">
+              <div class="flex justify-between">
+                <span>Memory</span>
+                <div>
+                  8 GB RAM
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="flex flex-col w-full">
+            <div class="py-[10px]" style="border-bottom: 1px solid rgba(255, 255, 255, 0.1);}">
+              <div class="flex justify-between">
+                <span>Graphics</span>
+                <div>
+                  GTX 960, Radeon R9 380
+                </div>
+              </div>
+            </div>
+            <div class="flex flex-col w-full">
+              <div class="py-[10px]" style="border-bottom: 1px solid rgba(255, 255, 255, 0.1);}">
+                <div class="flex justify-between">
+                  <span>DirectX</span>
+                  <div>
+                    Version 11
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="flex flex-col w-full">
+              <div class="py-[10px]" style="border-bottom: 1px solid rgba(255, 255, 255, 0.1);}">
+                <div class="flex justify-between">
+                  <span>Storage</span>
+                  <div>
+                    65 GB available space
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
